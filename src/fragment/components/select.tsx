a947c622edc241cf423011e74a5847cb8871d7f3
ashabehr <ashabehrbakhshaee@yahoo.com>
2025-11-22 09:49:07 +0330

    Update select.tsx

--- src/fragment/components/select.tsx
+++ src/fragment/components/select.tsx
@@ -107,16 +107,7 @@ export const selectMeta: CodeComponentMeta<SelectType> = {
             advanced: true,
           },
         },
-        defaultValue: [
-          {
-            label: "Option 1",
-            value: "option1",
-          },
-          {
-            label: "Option 2",
-            value: "option2",
-          },
-        ],
+        defaultValue: "option1",
         exprHint:
           'An array of items, like `["Option1", "Option2"]`, or an array of objects with `value`, `label`, or `disabled`, like `[{value: "usa", label: "United States"}, {value: "bra", label: "Brazil"}]`',
       },
