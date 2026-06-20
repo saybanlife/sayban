28a9eb00d70ded27907798492ca7fe67c78829ab
saybanlife <sayban.life@gmail.com>
2026-06-20 09:37:09 +0330

    Update fragment.tsx

--- src/fragment/fragment.tsx
+++ src/fragment/fragment.tsx
@@ -95,7 +95,7 @@ export const Fragment = ({
         const expires = new Date(Date.now() + days * 864e5).toUTCString();
         document.cookie = `${name}=${encodeURIComponent(
           value
-        )}; expires=${expires}; path=/; domain=.darkube.ir; secure; SameSite=Lax`;
+        )}; expires=${expires}; path=/; domain=.sayban.app; secure; SameSite=Lax`;
       },
 
       getCookie: (name: string) => {
