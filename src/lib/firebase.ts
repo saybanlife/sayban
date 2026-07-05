21546cd35474a92151cbd10a9c83a9321aeb70c8
saybanlife <sayban.life@gmail.com>
2026-07-05 10:09:29 +0330

    Create firebase.ts

---
+++ src/lib/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBVtKyIzcD0xVEMOjeMYjDdNRozFVVrmRo",
+  authDomain: "liom-31952.firebaseapp.com",
+  databaseURL: "https://liom-31952.firebaseio.com",
+  projectId: "liom-31952",
+  storageBucket: "liom-31952.firebasestorage.app",
+  messagingSenderId: "518322220404",
+  appId: "1:518322220404:web:09527c8a42f2f017d89021",
+  measurementId: "G-TVWYWYEH1D",
+};
+
+export const firebaseApp =
+  getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
