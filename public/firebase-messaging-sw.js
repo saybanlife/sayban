f511a2aefff63ac1afabd0389d6c44b7b298fd7c
saybanlife <sayban.life@gmail.com>
2026-07-05 10:58:03 +0330

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,23 +1,20 @@
 /* eslint-disable no-undef */
-
 importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js");
-importScripts(
-  "https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js"
-);
+importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAG_UJ8y1wWjqJT4LnFKT3O6bWGjd7CORI",
+  authDomain: "sayban-web.firebaseapp.com",
+  projectId: "sayban-web",
+  storageBucket: "sayban-web.firebasestorage.app",
+  messagingSenderId: "856382957880",
+  appId: "1:856382957880:web:13290f4f3da5805ac179cd"
 });
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  console.log("Background message:", payload);
+  console.log("Background message received:", payload);
 
   const notificationTitle = payload?.notification?.title || "پیام جدید";
   const notificationOptions = {
