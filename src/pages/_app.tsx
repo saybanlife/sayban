df4b9812bdbf80a8d8ea401f4e5c6ca87adf6da3
saybanlife <sayban.life@gmail.com>
2026-07-05 10:17:52 +0330

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,19 @@
 import "../../components/plasmic/sayban/plasmic.css"; // plasmic-import: qARqpE4p5tZmJuNxFbTaPz/projectcss
 import "@/styles/globals.css";
 import "@/styles/date-picker.css";
+
+import { useEffect } from "react";
 import { PlasmicRootProvider } from "@plasmicapp/react-web";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+
+import { initFcm } from "@/lib/fcm";
+
 export default function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    initFcm();
+  }, []);
+
   return (
     <PlasmicRootProvider Head={Head}>
       <Component {...pageProps} />
