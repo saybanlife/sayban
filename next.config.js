cf78dd27c28826996afe26a1ad3febcea96814f3
saybanlife <sayban.life@gmail.com>
2026-07-05 10:29:58 +0330

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -6,4 +13,4 @@ const nextConfig = {
   trailingSlash: true,
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
